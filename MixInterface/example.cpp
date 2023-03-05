5ad7888aac88e9a19ef774d3da5e62db97798f22
xxl <3161932956@qq.com>
2023-03-05 12:02:57 +0800

    Add The Mixture of Python and c++ (Pybind11 and Cython)

---
+++ MixInterface/example.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(example, m) {
+m.doc() = "pybind11 example plugin"; // optional module docstring
+
+m.def("add", &add, "A function that adds two numbers");
+}
+
+//c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
