b7b1753efe308d7d3fd2df1659e0b20eca6da102
xxl <3161932956@qq.com>
2023-04-09 21:25:37 +0800

    quickly start the cmake

---
+++ MixInterface/CmakeTest/Mulmain.cpp
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "math/Math.h"
+using namespace std;
+
+int main(int argc, char *argv[])  //本身的命令会当作第一个参数
+{
+    if (argc < 3){
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+    double result = power(base, exponent);
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    return 0;
+}
+
+
